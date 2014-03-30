f322ea2e2474392ccd77b065d6cd0a2fcc25055b
Simon Tucker <brotherlogic@gmail.com>
2014-03-30 08:26:06 -0700

    First pass

---
+++ ShoppingListTests/ShoppingListTests.m
@@ -0,0 +1,34 @@
+//
+//  ShoppingListTests.m
+//  ShoppingListTests
+//
+//  Created by Simon Tucker on 30/03/2014.
+//  Copyright (c) 2014 Brotherlogic. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShoppingListTests : XCTestCase
+
+@end
+
+@implementation ShoppingListTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
